99ed914438fe61a9df7246af623e026eb675d793
Nico Archi <narchitector@gmail.com>
2020-08-04 09:50:23 +0200

    Updates

---
+++ precache-manifest.f0a68de0cfccc05918dedf2fccaa3ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de82ee1cce591b2b48c155cb5117531",
+    "url": "/camtracker/index.html"
+  },
+  {
+    "revision": "e3ae3665848bdbc1f706",
+    "url": "/camtracker/static/css/main.fb00a589.chunk.css"
+  },
+  {
+    "revision": "28ca26da54562904b7c0",
+    "url": "/camtracker/static/js/2.feb21543.chunk.js"
+  },
+  {
+    "revision": "7c7609abcd51a48d6f6f13dfa5bc77b7",
+    "url": "/camtracker/static/js/2.feb21543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ae3665848bdbc1f706",
+    "url": "/camtracker/static/js/main.fb21b145.chunk.js"
+  },
+  {
+    "revision": "d8ef8643233b45484469",
+    "url": "/camtracker/static/js/runtime-main.d0c21e61.js"
+  }
+]);
